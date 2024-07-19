de212e765bf42408d2c873c7b81b3f4ebe16084d
shiva <shivamotog62@gmail.com>
2024-07-19 20:47:51 +0530

    asdfds

--- index.mjs
+++ index.mjs
@@ -1,12 +1,12 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 4000;
+const portSelected = process.env.PORT || 4000;
 
 import mysql from "mysql2/promise";
 
 // Create the connection to database
 const connection = await mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "password",
 });
@@ -15,7 +15,6 @@ const connection = await mysql.createConnection({
 
 try {
   const [results] = await connection.query("SHOW DATABASES");
-
   console.log(results);
 } catch (err) {
   console.log(err);
@@ -25,6 +24,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(portSelected, () => {
+  console.log(`Example app listening on port ${portSelected}`);
 });
