5550f2cfb875945dc784f63374e4bcc5b129ba8f
shiva <shivamotog62@gmail.com>
2024-07-19 19:51:54 +0530

    asf

--- index.mjs
+++ index.mjs
@@ -1,24 +1,6 @@
 import express from "express";
 const app = express();
 
-import mysql from "mysql2/promise";
-
-// Create the connection to database
-const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-});
-
-// A simple SELECT query
-try {
-  const [results] = await connection.query("SHOW DATABASES");
-
-  console.log(results);
-} catch (err) {
-  console.log(err);
-}
-
 app.get("/", (req, res) => {
   res.send("hello world");
 });
