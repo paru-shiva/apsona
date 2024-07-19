a1894bc1a0ac762409e2953ea408fdd0e1094eaa
shiva <shivamotog62@gmail.com>
2024-07-19 20:03:49 +0530

    dasf

--- index.mjs
+++ index.mjs
@@ -2,6 +2,25 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 4000;
 
+import mysql from "mysql2/promise";
+
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+});
+
+// A simple SELECT query
+
+try {
+  const [results] = await connection.query("SHOW DATABASES");
+
+  console.log(results);
+} catch (err) {
+  console.log(err);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
