c588f3a51d8f0db7636c26708adfd49e92358efd
shiva <shivamotog62@gmail.com>
2024-07-19 20:51:20 +0530

    asdfds

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ import mysql from "mysql2/promise";
 
 // Create the connection to database
 const connection = await mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root",
   password: "password",
 });
